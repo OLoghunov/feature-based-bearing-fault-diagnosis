039d78560d0b6cb5f9d20e23a686dc69168edb88
OLoghunov <loghunov1@gmail.com>
2025-05-19 00:17:36 +0300

    misc changes

--- DWT_GGD_Paderborn/main.m
+++ DWT_GGD_Paderborn/main.m
@@ -2,9 +2,6 @@
 % 
 % Сheck out the path variable in load_data function
 % 
-% If the number of features has changed, 
-% you need to change the variable N_features in load_data respectively
-% 
 % load_data contains RMS normalization of signal
 % change it if necessary
 % 
@@ -15,15 +12,19 @@ warning('off')
 close all
 
 % Data parameters
-num_samples = 2400;
-num_features = 4;
-signal_sizes = [1000];
+cfg = config();
+
+n_folders = 8;
+n_samples_per_folder = 400;
+n_samples = n_folders * n_samples_per_folder;
+n_features = cfg.n_features;
+signal_sizes = [10000];
 accuracy = zeros(1, length(signal_sizes));
 
-feature_names = strcat('feature_', string(1:num_features));
-features = array2table(zeros(num_samples, num_features), ...
+feature_names = strcat('feature_', string(1:n_features));
+features = array2table(zeros(n_samples, n_features), ...
            'VariableNames', feature_names);
-features.response = strings(num_samples, 1);
+features.response = strings(n_samples, 1);
 
 current_idx = 1;
     
@@ -32,39 +33,50 @@ for i = 1:length(signal_sizes)
     features{:, :} = 0;
     features.response(:) = "";  
 
-    num_samples_per_file = 400;
-
     % Healthy
-    % varnames = ["K001","K002","K003","K006"];
-    varnames = ["K001","K002"];
+    varnames = ["K001","K002","K003","K004","K005"];
+%     varnames = [];
     fault_type = "Healthy";
     features = load_data_into_table(features, varnames, fault_type, ...
-                                   signal_sizes(i), num_samples_per_file, current_idx);
-    current_idx = current_idx + length(varnames)*num_samples_per_file;
+                                   signal_sizes(i), ...
+                                   n_samples_per_folder, current_idx, cfg);
+    current_idx = current_idx + length(varnames)*n_samples_per_folder;
 
     
     % OR Damage
-    % varnames = ["KA04","KA05","KA22","KA30"];
-    varnames = ["KA04","KA05"];
+    varnames = ["KA04","KA05","KA22","KA30"];
+%     varnames = ["KB27"];
     fault_type = "ORDamage";
     features = load_data_into_table(features, varnames, fault_type, ...
-                                   signal_sizes(i), num_samples_per_file, current_idx);
-    current_idx = current_idx + length(varnames)*num_samples_per_file;
+                                   signal_sizes(i), ...
+                                   n_samples_per_folder, current_idx, cfg);
+    current_idx = current_idx + length(varnames)*n_samples_per_folder;
     
     % IR Damage
-    % varnames = ["KI01","KI16","KI17","KI21"];
-    varnames = ["KI01","KI16"];
+    varnames = ["KI01","KI16","KI17","KI21"];
+%     varnames = ["KB23","KB24"];
     fault_type = "IRDamage";
     features = load_data_into_table(features, varnames, fault_type, ...
-                                   signal_sizes(i), num_samples_per_file, current_idx);   
+                                   signal_sizes(i), ...
+                                   n_samples_per_folder, current_idx, cfg);   
     
-    % KNN    
-    [~, accuracy(i)] = train_classifier(features);
+    % Training    
+    [~, accuracy(i)] = train_ensemble(features);
 end
 
-plot(signal_sizes, accuracy)
-xlabel('sample number')
-ylabel('accuracy')
-grid on
+plot(signal_sizes, accuracy, '-o', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerFaceColor', 'w');
+xlabel('Signal length (samples)', 'FontSize', 12);
+ylabel('Accuracy', 'FontSize', 12);
+grid on;
+ylim([min(accuracy)-0.05, max(accuracy)+0.1]);
+
+text_offset = (max(accuracy) - min(accuracy)) * 0.05;
+
+for i = 1:length(signal_sizes)
+    text(signal_sizes(i), accuracy(i) + text_offset, ...
+        sprintf('%.1f%%', accuracy(i)*100), ...
+        'FontSize', 10, ...
+        'HorizontalAlignment', 'center');
+end
 
 warning('on')
