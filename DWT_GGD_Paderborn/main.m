2b8367bbf54510d4bc66d29866e530410432e903
OLoghunov <loghunov1@gmail.com>
2025-05-18 18:28:58 +0300

    added preallocation for features table

--- DWT_GGD_Paderborn/main.m
+++ DWT_GGD_Paderborn/main.m
@@ -1,46 +1,65 @@
 % MAIN SCRIPT
 % 
-% Сheck out the path variable in LoadFromK function
+% Сheck out the path variable in load_data function
 % 
 % If the number of features has changed, 
-% you need to change the variable N_features in LoadFromK respectively
+% you need to change the variable N_features in load_data respectively
 % 
-% LoadFromK contains RMS normalization of signal
+% load_data contains RMS normalization of signal
 % change it if necessary
 % 
-% Output of GetFeatures function is array of doubles
+% Output of get_features function is array of doubles
 
-% Warnings occur from the fact that parameters in fitGGD code are changing
+% Warnings occur from the fact that parameters in fit_GGD code are changing
 warning('off')
-
 close all
-features = table();
 
-signal_sizes = [1000, 2000, 5000];
+% Data parameters
+num_samples = 2400;
+num_features = 4;
+signal_sizes = [1000];
 accuracy = zeros(1, length(signal_sizes));
 
+feature_names = strcat('feature_', string(1:num_features));
+features = array2table(zeros(num_samples, num_features), ...
+           'VariableNames', feature_names);
+features.response = strings(num_samples, 1);
+
+current_idx = 1;
+    
 for i = 1:length(signal_sizes)
+    current_idx = 1;
+    features{:, :} = 0;
+    features.response(:) = "";  
+
+    num_samples_per_file = 400;
+
     % Healthy
-    varnames = ["K001","K002","K003","K006"];
-    faultType = "Healthy";
-    features = [features; LoadFromK(varnames, faultType, signal_sizes(i))];
+    % varnames = ["K001","K002","K003","K006"];
+    varnames = ["K001","K002"];
+    fault_type = "Healthy";
+    features = load_data_into_table(features, varnames, fault_type, ...
+                                   signal_sizes(i), num_samples_per_file, current_idx);
+    current_idx = current_idx + length(varnames)*num_samples_per_file;
+
     
     % OR Damage
-    varnames = ["KA04","KA05","KA22","KA30"];
-    faultType = "ORDamage";
-    features = [features; LoadFromK(varnames, faultType, signal_sizes(i))];
+    % varnames = ["KA04","KA05","KA22","KA30"];
+    varnames = ["KA04","KA05"];
+    fault_type = "ORDamage";
+    features = load_data_into_table(features, varnames, fault_type, ...
+                                   signal_sizes(i), num_samples_per_file, current_idx);
+    current_idx = current_idx + length(varnames)*num_samples_per_file;
     
     % IR Damage
-    varnames = ["KI01","KI16","KI17","KI21"];
-    faultType = "IRDamage";
-    features = [features; LoadFromK(varnames, faultType, signal_sizes(i))];
-    
-    features.Properties.VariableNames{end} = 'response';   
+    % varnames = ["KI01","KI16","KI17","KI21"];
+    varnames = ["KI01","KI16"];
+    fault_type = "IRDamage";
+    features = load_data_into_table(features, varnames, fault_type, ...
+                                   signal_sizes(i), num_samples_per_file, current_idx);   
     
     % KNN    
-    [~, accuracy(i)] = trainClassifier(features);
-
-     features = table();
+    [~, accuracy(i)] = train_classifier(features);
 end
 
 plot(signal_sizes, accuracy)
