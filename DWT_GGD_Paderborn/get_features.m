7d69ca639a1ca71bda46b078e5805c5302997f69
OLoghunov <loghunov1@gmail.com>
2025-05-19 00:17:20 +0300

    added 23 features (Lessmeier et al., 2016)

--- DWT_GGD_Paderborn/get_features.m
+++ DWT_GGD_Paderborn/get_features.m
@@ -1,57 +1,73 @@
-function fts = get_features(signal)
-    %% TIME DOMAIN
-    % peak value
-    %     
-%     signalPeak = max(signal);
-%     
-%     % shape factor
-%     %     
-%     shapeFactor = rms(signal)/mean(abs(signal));
+function fts = get_features(signal, fs, rpm)
+    %% PREPROCESSING
 
+    % Bearing Parameters (Table 1)
+    Nb = 8;
+    Bd = 6.75e-3;
+    Pd = 28.55e-3;
+    phi = 0;
+
+    bpfo = (Nb/2) * (1 - (Bd/Pd)*cos(phi)) * (rpm/60);
+    bpfi = (Nb/2) * (1 + (Bd/Pd)*cos(phi)) * (rpm/60);
     
-    %% WAVELET DOMAIN
-    % decomposition level of signal
-    %
-    i = 5; 
-    levels = [1 2];
-    fts_on_level = 2;
+    %%
 
-    fts = zeros(1, length(levels) * fts_on_level);
-    normalized_coefs = cell(1, length(levels));
+    % Bands around BPFO/BPFI ±20%
+    [b, a] = butter(4, [0.8*bpfo, 1.2*bpfo]/(fs/2), 'bandpass');
+    bpfo_signal = filtfilt(b, a, signal);
+    [b, a] = butter(4, [0.8*bpfi, 1.2*bpfi]/(fs/2), 'bandpass');
+    bpfi_signal = filtfilt(b, a, signal);
 
-    % wavelet coefficients
-    %
-    [c,l] = wavedec(signal,i,"sym3");
+    wp = wpdec(signal, 3, 'db4');
     
-    detCoefs = detcoef(c,l,levels);
-%     approx = appcoef(c,l,"db2");
-
-    for i = 1:length(levels)
-        approx = cell2mat(detCoefs(i));
-
-        wave_norm = (approx - min(approx)) / ( max(approx) - min(approx));
-        wave_norm = wave_norm + 0.00001;
-        wave_norm = rmoutliers(wave_norm, 'median');
-        wave_norm = wave_norm';
-
-        normalized_coefs(i) = {wave_norm};
+    % Energy of 8 nodes at 3rd level
+    wavelet_features = zeros(1,8);
+    for i = 0:7
+        node_coefs = wpcoef(wp, [3 i]);
+        wavelet_features(i+1) = sum(node_coefs.^2); % Энергия узла
     end
 
-    % energy of the wavelet coefficients
-    %
-%     waveletEnergy = wenergy(c, l);
-
-
-    %% STATISTICAL ANALYSIS
-    % distribution
-    %
-    for i = 1:length(normalized_coefs)
-%         pd(i) = fitdist(cell2mat(normalizedCoefs(i)), 'normal');
-        pd = fit_GGD(cell2mat(normalized_coefs(i)));
-
-        fts(fts_on_level * i - 1) = pd.a;
-        fts(fts_on_level * i) = pd.b;
-%         fts(ftsOnLevel * i) = pd.m;
+    [P1, f] = pwelch(signal, [], [], [], fs);
+    
+    % BPFO Harmonic Search (1-3 harmonics)
+    bpfo_harmonics = zeros(1,3);
+    for i = 1:3
+        [~, idx] = findpeaks(P1, 'SortStr','descend', ...
+            'NPeaks',1, 'MinPeakDistance',bpfo*0.1);
+        bpfo_harmonics(i) = P1(idx(1));
     end
+    
+    % BPFI Harmonic Search (1-3 harmonics)
+    bpfi_harmonics = zeros(1,3);
+    for i = 1:3
+        [~, idx] = findpeaks(P1, 'SortStr','descend', ...
+            'NPeaks',1, 'MinPeakDistance',bpfi*0.1);
+        bpfi_harmonics(i) = P1(idx(1));
+    end
+    
+    freq_features = [bpfo_harmonics, bpfi_harmonics];
+
+    % Envelope through the Hilbert transform
+    env_bpfo = abs(hilbert(bpfo_signal));
+    env_bpfi = abs(hilbert(bpfi_signal));
+    
+    envelope_features = [
+        rms(env_bpfo), rms(env_bpfi), kurtosis(env_bpfo), entropy(env_bpfi), peak2peak(env_bpfo)
+    ];
+
+    % Depth of amplitude modulation
+    am_depth_bpfo = (max(bpfo_signal) - min(bpfo_signal)) / mean(abs(bpfo_signal));
+    am_depth_bpfi = (max(bpfi_signal) - min(bpfi_signal)) / mean(abs(bpfi_signal));
+    
+    % Energy ratio
+    energy_ratio = sum(bpfo_signal.^2) / sum(bpfi_signal.^2);
+    
+    mod_features = [
+        am_depth_bpfo, am_depth_bpfi, energy_ratio, mean(abs(diff(signal)))
+    ];
+
+    fts = [wavelet_features, freq_features, envelope_features, mod_features];
+%     fts = wavelet_features([2, 4, 6, 8]);
+
 end
 
