608717947a0eae32c03e0ec0894dc2b6fc3b7f66
OLoghunov <loghunov1@gmail.com>
2025-05-29 18:51:06 +0300

    misc

--- DWT_GGD_Paderborn/config.m
+++ DWT_GGD_Paderborn/config.m
@@ -1,4 +1,7 @@
 function cfg = config()
     cfg = struct();
-    cfg.n_features = 23;
+    mock_x = linspace(-5, 5, 100);
+    mock_y = sin(mock_x);
+    n = length(get_features(mock_y, 1, 1));
+    cfg.n_features = n;
 end
